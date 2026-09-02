4a7ad9eed0eabd5616729b0ef5d3990b55d2380f
Nikhil Bhatt <nikhilbhatt7890@gmail.com>
2026-09-02 12:00:44 +0530

    Implement encyclopedia page rendering and update styles
    - Add page view to render Markdown content for entries
    - Update index.html to link entries
    - Enhance styles for textarea and layout
    - Create page.html template for individual entries
    - Update .gitignore to include Guide.txt

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-
+from markdown import markdown
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+
+def page(request,title):
+    
+    with open(f"entries/{title}.md",'r') as f :
+        content = f.read()
+
+    html = markdown(content)
+    return render(request,"encyclopedia/page.html",{
+        "content":html
     })
 
