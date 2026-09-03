21ce079a6eea28d8472d4344739f8cb645b08d83
Nikhil Bhatt <nikhilbhatt7890@gmail.com>
2026-09-03 13:17:38 +0530

    Add search functionality and enhance header styling in encyclopedia app

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from . import views
-
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search",views.search,name="search"),
     path("<str:title>",views.page)
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from markdown import markdown
-from . import util
+from . import util 
 
+
+
+
+def r_render(request,header):
+    return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "header":header
+        })
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return r_render(request,"")
 
 
 def page(request,title):
-    
+    print("page",title)
     with open(f"entries/{title}.md",'r',encoding="utf-8") as f :
         content = f.read()
 
@@ -19,3 +25,19 @@ def page(request,title):
         "content":html
     })
 
+def search(request):
+    entries = util.list_entries()
+    merged = "".join(entries)
+    if request.method == "POST" :
+        task = request.POST["q"].upper()
+        
+        if task not in merged:
+           return r_render(request,"No matching entry found!")
+        
+        entries = [val for val in entries if task in val]
+
+        # print(task*100)
+        # print(entries)
+        # print(entries)
+    return r_render(request,"!Error Internal server Error!!! refector the all pages to most closest pages ")
+    
